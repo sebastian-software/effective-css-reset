453dc9ef4bf3deafa889c9d904ac32d60f01fa80
Sebastian Werner <s.werner@sebastian-software.de>
2024-03-07 11:11:51 +0100

    feat: added fieldset and a good amount of different form fields

--- src/stories/Form.tsx
+++ src/stories/Form.tsx
@@ -1,28 +1,59 @@
 import { CodeDoc } from "./util/CodeDoc"
 
+function noChange() {}
+
 export function Form() {
   return (
-    <>
-      <p>
-        <input type="text" value="Input Text" onChange={() => null} />
+    <fieldset>
+      <legend>Form Demo</legend>
+      <div>
+        <input type="text" defaultValue="Input Text" />
         <CodeDoc>input type=text</CodeDoc>
-      </p>
-      <p>
+      </div>
+      <div>
+        <input type="search" defaultValue="Input Search" />
+        <CodeDoc>input type=search</CodeDoc>
+      </div>
+      <div>
+        <input type="button" defaultValue="Input Button" />
+        <CodeDoc>input type=button</CodeDoc>
+      </div>
+      <div>
+        <input type="number" defaultValue="336699" />
+        <CodeDoc>input type=number</CodeDoc>
+      </div>
+      <div>
+        <input type="date" />
+        <CodeDoc>input type=date</CodeDoc>
+      </div>
+      <div>
+        <input type="datetime-local" />
+        <CodeDoc>input type=datetime-local</CodeDoc>
+      </div>
+      <div>
+        <input type="color" defaultValue="#6FF" />
+        <CodeDoc>input type=color</CodeDoc>
+      </div>
+      <div>
+        <input type="file" />
+        <CodeDoc>input type=file</CodeDoc>
+      </div>
+      <div>
         <select>
           <option>Value</option>
         </select>
         <CodeDoc>select</CodeDoc>
-      </p>
-      <p>
+      </div>
+      <div>
         <input type="checkbox" id="chk1" />
         <label htmlFor="chk1">Label for Checkbox</label>
         <CodeDoc>input type=checkbox</CodeDoc>
-      </p>
-      <p>
+      </div>
+      <div>
         <input type="radio" id="rdi1" />
         <label htmlFor="rdi1">Label for Radio Box</label>
         <CodeDoc>input type=radio</CodeDoc>
-      </p>
-    </>
+      </div>
+    </fieldset>
   )
 }
