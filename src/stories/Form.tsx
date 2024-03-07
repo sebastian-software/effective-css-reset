97db9838afe02da7ed6d844843a7effacf40d642
Sebastian Werner <s.werner@sebastian-software.de>
2024-03-07 12:34:09 +0100

    refactor: use new InlineWrapper demo component

--- src/stories/Form.tsx
+++ src/stories/Form.tsx
@@ -1,59 +1,66 @@
-import { CodeDoc } from "./util/CodeDoc"
-
-function noChange() {}
+import { CodeDoc, InlineWrapper } from "./util/CodeDoc"
 
 export function Form() {
   return (
     <fieldset>
-      <legend>Form Demo</legend>
-      <div>
+      <legend>Fieldset Legend</legend>
+      <InlineWrapper>
         <input type="text" defaultValue="Input Text" />
         <CodeDoc>input type=text</CodeDoc>
-      </div>
-      <div>
+      </InlineWrapper>
+      <InlineWrapper>
         <input type="search" defaultValue="Input Search" />
         <CodeDoc>input type=search</CodeDoc>
-      </div>
-      <div>
+      </InlineWrapper>
+      <InlineWrapper>
         <input type="button" defaultValue="Input Button" />
         <CodeDoc>input type=button</CodeDoc>
-      </div>
-      <div>
+      </InlineWrapper>
+      <InlineWrapper>
         <input type="number" defaultValue="336699" />
         <CodeDoc>input type=number</CodeDoc>
-      </div>
-      <div>
+      </InlineWrapper>
+      <InlineWrapper>
+        <input type="range" />
+        <CodeDoc>input type=range</CodeDoc>
+      </InlineWrapper>
+      <InlineWrapper>
         <input type="date" />
         <CodeDoc>input type=date</CodeDoc>
-      </div>
-      <div>
+      </InlineWrapper>
+      <InlineWrapper>
         <input type="datetime-local" />
         <CodeDoc>input type=datetime-local</CodeDoc>
-      </div>
-      <div>
+      </InlineWrapper>
+      <InlineWrapper>
+        <input type="time" />
+        <CodeDoc>input type=time</CodeDoc>
+      </InlineWrapper>
+      <InlineWrapper>
         <input type="color" defaultValue="#6FF" />
         <CodeDoc>input type=color</CodeDoc>
-      </div>
-      <div>
+      </InlineWrapper>
+      <InlineWrapper>
         <input type="file" />
         <CodeDoc>input type=file</CodeDoc>
-      </div>
-      <div>
+      </InlineWrapper>
+
+      <InlineWrapper>
         <select>
           <option>Value</option>
         </select>
         <CodeDoc>select</CodeDoc>
-      </div>
-      <div>
+      </InlineWrapper>
+      <InlineWrapper>
         <input type="checkbox" id="chk1" />
         <label htmlFor="chk1">Label for Checkbox</label>
         <CodeDoc>input type=checkbox</CodeDoc>
-      </div>
-      <div>
+      </InlineWrapper>
+      <InlineWrapper>
         <input type="radio" id="rdi1" />
         <label htmlFor="rdi1">Label for Radio Box</label>
         <CodeDoc>input type=radio</CodeDoc>
-      </div>
+      </InlineWrapper>
     </fieldset>
   )
 }
--- src/stories/util/CodeDoc.tsx
+++ src/stories/util/CodeDoc.tsx
@@ -1,14 +1,24 @@
 import { PropsWithChildren } from "react"
 
+const inlineStyle = {
+  display: "flex",
+  flexDirection: "row",
+  alignItems: "center",
+  gap: "0.5rem",
+  paddingBlock: "0.25rem"
+}
+
 const codeStyle = {
   display: "inline-block",
   fontSize: "0.75rem",
   color: "rgba(0,0,0,0.7)",
   background: "lightblue",
   padding: "0.125rem 0.25rem",
-  borderRadius: "4px",
-  marginTop: "1rem",
-  marginLeft: "0.5rem"
+  borderRadius: "4px"
+}
+
+export function InlineWrapper({ children }: PropsWithChildren) {
+  return <div style={inlineStyle}>{children}</div>
 }
 
 export function CodeDoc({ children }: PropsWithChildren) {
